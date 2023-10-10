1023d9ebed273646899d66722aeaf0a3e994b4a2
Jeb Bearer <jeb@espressosys.com>
2023-10-10 17:06:56 +0200

    Make RPC ports configurable for all commands

--- src/cli.ts
+++ src/cli.ts
@@ -6,13 +6,13 @@ import { Reorgme, ReorgmeDefaults } from './reorgme'
 
 export const command = yargs(hideBin(process.argv))
   .options({
-    id: { type: 'number', default: ReorgmeDefaults.id }
+    id: { type: 'number', default: ReorgmeDefaults.id },
+    rpcPort: { type: 'array', help: "RPC ports for the 3 geth nodes", default: [18545, 28545, 38545] }
   })
   .command("start", "creates and starts a new testnet blockchain", (yargs) => {
     yargs.options({
       detach: { type: 'boolean', default: true },
       allocation: { type: 'array', help: "balance allocation on genesis, e.g. 0xf41c74c9ae680c1aa78f42e5647a62f353b7bdde=1000000000000000000", default: [] },
-      rpcPort: { type: 'array', help: "RPC ports for the 3 geth nodes", default: [18545, 28545, 38545] }
     })
   }, async (args) => {
     const allocations = (args.allocation as string[]).map((s) => {
@@ -23,12 +23,7 @@ export const command = yargs(hideBin(process.argv))
       return { addr: parts[0], balance: parts[1] }
     }).reduce((p, v) => ({ ...p, [v.addr]: { balance: v.balance }}), {})
 
-    const rpcPorts = args.rpcPort as number[]
-    if (rpcPorts.length != 3) {
-      throw new Error(`must specify exactly 3 RPC ports`)
-    }
-
-    const reorgme = new Reorgme({ id: args.id, allocations: allocations, rpcPorts: rpcPorts })
+    const reorgme = new Reorgme({ id: args.id, allocations: allocations, rpcPorts: parseRpcPorts(args) })
 
     let canceled = false
 
@@ -51,28 +46,36 @@ export const command = yargs(hideBin(process.argv))
     }
   })
   .command("stop", "stops and removes a testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).stop()
+    await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).stop()
   })
   .command("pause", "pauses a testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).pause()
+    await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).pause()
   })
   .command("resume", "resumes a testnet blockchain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).resume()
+    await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).resume()
   })
   .command("fork", "forks the node 0 of the chain", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).fork()
+    await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).fork()
   })
   .command("join", "joins the node 0 after forking it", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).join()
+    await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).join()
   })
   .command("logs", "show logs for all the nodes", () => {}, async (args) => {
-    await new Reorgme({ id: args.id }).logs()
+    await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).logs()
   })
   .command("node", "node related commands", (yargs) => yargs
     .options({
       index: { type: 'number', require: true }
     })
     .command("ip", "retrieve the ip of a node", () => {}, async (args) => {
-      console.log(await new Reorgme({ id: args.id }).ipOf(args.index))
+      console.log(await new Reorgme({ id: args.id, rpcPorts: parseRpcPorts(args) }).ipOf(args.index))
     }))
   .parse()
+
+function parseRpcPorts (args: { rpcPort: number[] }): number[] {
+  const rpcPorts = args.rpcPort as number[]
+  if (rpcPorts.length != 3) {
+    throw new Error(`must specify exactly 3 RPC ports`)
+  }
+  return rpcPorts
+}
